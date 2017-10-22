cb28c4045bd0f252e447470894959f89f0619002
Wolfleader101 <james.morgan@iinet.net.au>
2017-10-22 15:41:39 +0800

    Added all files!

---
+++ main.js
@@ -0,0 +1,23 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    'outerBounds': {
+      'width': 400,
+      'height': 500
+    }
+  });
+});
+
+const memeNames = ['meme1.jpg', 'meme2.png'];
+
+const memeImgs = [];
+
+for (let i = 0; i < memeNames.length; i++) {
+    memeImgs[i] = new Image();
+    memeImgs[i].src = 'Memes/' + memeNames[i];
+};
+
+for (img in document.getElementsByTagName('img')) {
+    img.src = memeImgs[Math.floor(Math.random() * memeImgs.length)].src;
+}
+
+alert("meme");
