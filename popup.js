72e982d9852d315d5993c9cb690f06f2bb175cd2
Wolfleader <james.morgan@iinet.net.au>
2020-02-14 22:02:03 +0800

    random images work. text work on getting images to be replaced

--- popup.js
+++ popup.js
@@ -1,20 +1,13 @@
-  const memeNames = ['meme1.jpg', 'meme2.png'];
-  const memeImgs = {};
+const memeNames = ['meme1.jpg', 'meme2.png'];
 
-  memeNames.forEach(el => {
-    memeImgs[el] = new Image();
-  Object.values(memeImgs).src = './Memes/' + memeNames[el];
-   console.log(memeImgs)
-   document.body.appendChild(Object.values(memeImgs))
-  })
+// get random picture
+document.getElementById('imgs').innerHTML = `<img width='100' height='200' src="./Memes/${memeNames[Math.floor(Math.random() * memeNames.length)]}"></img>`;
 
-  DocImgs = document.getElementsByTagName('img');
-  //console.log(DocImgs)
-
-  for(let i in DocImgs) {
-    i.src = memeImgs[Math.floor(Math.random() * memeImgs.length)].src;
-  }
-  
-  console.log(document.getElementsByTagName('img'));
-  console.log(memeImgs);
-  //window.onload = function() {}
+DocImgs = document.getElementsByTagName('img');
+console.log(DocImgs)
+DocImgs.forEach(el => {
+  randImg = `<img width='100' height='200' src="./Memes/${memeNames[Math.floor(Math.random() * memeNames.length)]}"></img>`
+  console.log(randImg)
+  console.log(i)
+  el.parentNode.replaceChild(randImg, el)
+});
