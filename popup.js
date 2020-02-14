4723c5d7485785d00c3669ee45ef11bcd05a5ab3
Wolfleader <james.morgan@iinet.net.au>
2020-02-14 21:30:30 +0800

    trying to update this...

---
+++ popup.js
@@ -0,0 +1,23 @@
+setTimeout( () => {
+  const memeNames = ['meme1.jpg', 'meme2.png'];
+  const memeImgs = {};
+
+  memeNames.forEach(el => {
+    memeImgs[el] = new Image();
+  Object.values(memeImgs).src = './Memes/' + memeNames[el];
+   console.log(memeImgs)
+   document.body.appendChild(Object.values(memeImgs))
+  })
+
+  DocImgs = document.getElementsByTagName('img');
+  //console.log(DocImgs)
+
+  for(let i in DocImgs) {
+    i.src = memeImgs[Math.floor(Math.random() * memeImgs.length)].src;
+  }
+  
+  console.log(document.getElementsByTagName('img'));
+  console.log(memeImgs);
+  //window.onload = function() {}
+  
+}, 3000)
